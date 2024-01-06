69e78d8aba3c06efadb78e3b9ca7c37093b687c6
Bee <bee@bees-mbp-2.lan>
2024-01-06 11:41:47 -0500

    working

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 slint::include_modules!();
+use std::fs;
 use rfd::FileDialog;
-
+use pdf_extract::extract_text;
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
-
     let ui_handle = ui.as_weak();
     ui.on_request_increase_value(move || {
         let ui = ui_handle.unwrap();
@@ -13,23 +13,45 @@ fn main() -> Result<(), slint::PlatformError> {
     // ui.on_submit_url(move |url| {
     //    let ui: AppWindow = ui_handle.unwrap();
     //     ui.set_url(url);
-        
     // });
-
 
     let ui_handle = ui.as_weak();
     ui.on_open_file(move || {
-       let ui: AppWindow = ui_handle.unwrap();
-        // ui.set_url(url);
+    let ui: AppWindow = ui_handle.unwrap();
+    
         let files = FileDialog::new()
-    .add_filter("pdf", &["pdf"])
-    .add_filter("rust", &["rs", "toml"])
-    .set_directory("/")
-    .pick_file();
-       println!("{:?}",files);
-    //    ui.set_url(files.unwrap());
+        .add_filter("pdf", &["pdf"])
+        .add_filter("rust", &["rs", "toml"])
+        .set_directory("/")
+        .pick_file();
+    //    println!("{:?}",files);
+        ui.set_status("Loading...".into());
+
+        ui.set_url(files.clone().unwrap().display().to_string().into());
+        let mut extracted_txt_vec = Vec::new();
+        if let Some(ref file)= files{
+        let extracted_txt = extract_text(file.clone()).expect("Error");
+         extracted_txt_vec.push(extracted_txt.clone());
+        //  println!("{:?}",extracted_txt_vec);
+        // println!("************** {} ***********",&file.display());
+       }
+    //    println!("{:?}",extracted_txt_vec);
+    let mut out_file_path = files.clone().unwrap().display().to_string();
+    // add .txt extension to file path
+    out_file_path.push_str(".txt");
+    
+    let out_file = FileDialog::new()
+    .set_directory(out_file_path.clone())
+    .set_file_name("out.txt")
+    .save_file();
+    // println!("{:?}",out_file);
+
+for line in extracted_txt_vec{
+    fs::write(out_file.clone().unwrap(), line).expect("Unable to write file");
+    
+};
+
+ui.set_status("Done".into());
     });
-
-
     ui.run()
 }
